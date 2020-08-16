6c33088b67a1ae4fa40ceef97830297ce4e213ae
Malik-G <Malik-G@users.noreply.github.com>
2020-08-16 16:59:12 -0500

    progress commit, first 3 sections set up

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -8,7 +8,8 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Button from '@material-ui/core/Button';
 import ProjectSection from './ProjectSection';
-//import './ProjectPage.css';
+// import whatevs from '../images/malikg.png';
+import './ProjectPage.css';
 
 // const styling = theme => ({
 //    classname:{
@@ -20,127 +21,102 @@ import ProjectSection from './ProjectSection';
 
 class ProjectPage extends Component {
 
-   state = {
-      expanded: null,
-   };
+  state = {
+    expanded: null,
+    flipped: false
+  };
 
-   handleChange = panel => (event, expanded) => {
-      this.setState({
-         expanded: panel
-      });
-   };
+  handleChange = panel => (event, expanded) => {
+    this.setState({
+      expanded: panel
+    });
+  };
 
-   componentDidMount() {
-      this.props.dispatch({ type: 'GET_PORTFOLIO' })
-      //this.props.dispatch({type: 'GET_PROJECT_TAGS', payload: 1})
-      //this.getPortfolio();
-   }
+  componentDidMount() {
+    this.props.dispatch({ type: 'GET_PORTFOLIO' })
+    //this.props.dispatch({type: 'GET_PROJECT_TAGS', payload: 1})
+    //this.getPortfolio();
+  }
 
-   // getPortfolio = () => {
-   //    this.props.dispatch({type: 'GET_PORTFOLIO'})
-   // }
+  // getPortfolio = () => {
+  //    this.props.dispatch({type: 'GET_PORTFOLIO'})
+  // }
 
-   getProjectTags = (id) => {
-      return (event) => {
-         this.props.dispatch({ type: 'GET_PROJECT_TAGS', payload: id })
+  getProjectTags = (id) => {
+    return (event) => {
+      this.props.dispatch({ type: 'GET_PROJECT_TAGS', payload: id })
 
-      }
-   }
+    }
+  }
 
-   toAdminPage = () => {
-      this.props.history.push('/admin');
-   }
+  flip = (event) => {
+    if (this.state.flipped) {
+      document.getElementById("flip-note-inner").style.transform = "rotateY(360deg)"
+      this.setState({flipped: false})
+    }
+    else {
+      document.getElementById("flip-note-inner").style.transform = "rotateY(180deg)"
+      this.setState({ flipped: true })
+    }
+    console.log(event.target)
+    // console.log(document.getElementById("flip-note-inner"))
+  }
 
-   render() {
+  toAdminPage = () => {
+    this.props.history.push('/admin');
+  }
 
-      const { classes } = this.props
-      //const { expanded } = this.state;
+  render() {
 
-      return (
-         <section className="container">
-				<div className="parallax-stpaul">
-					<div className="float-left">
-						<img className="myPic" src="images/malik_glass2.png" alt="Picture of Malik Glass" />
-					</div>
-					<h1 className="">
-						<span className="colorOrange">The </span>
-						<span className="colorSeaGreen"> Port</span>
-						<span className="colorLightBlue">f<span onClick={this.toAdminPage}>o</span></span>
-						<span className="colorTomato">lio</span>
-					</h1>
-					<div>
-						<a href="https://github.com/Malik-G">Link to my GITHUB</a>
-					</div>
-				</div>
+    const { classes } = this.props
+    //const { expanded } = this.state;
 
-					<div className="parallax-reciperiot"></div>
+    return (
+      <section id="">
 
+        <div className="parallax-stpaul-main">
+          <img className="myPic" src="images/malikg.png" alt="Picture of Malik Glass" />
+          <h1>Malik Glass</h1>
+        </div>
 
-					<div className="infoContainer">
-						<h2 className="colorOrange">Description</h2>
+        <div className="parallax-stpaul-trans"></div>
 
-						<p></p>
-					</div>
-					<div className="infoContainer">
-						<h2 className="colorSeaGreen">See Online</h2>
-						<a href=""><Button variant="contained" color="primary">CODE ON GITHUB</Button></a>
-						<a href=""><Button variant="contained" color="default">WEBSITE</Button></a>
-					</div>
-					<div className="infoContainer">
-						<h2 className="colorTomato">Technologies</h2>
-						{this.props.reduxState.tags.map(tag =>
-							<span>*{tag.name} </span>
-						)}
-					</div>
-					<div className="parallax-rgb"></div>
-					{/* <img className="screenshot"  src="./images/rgb_game.png" alt="To-Do List screenshot" /> */}
+        <div className="bio-container">
+          <h1 className="big-font">
+            <span className="colorOrange">B</span>
+            <span className="colorSeaGreen">i</span>
+            <span className="colorTomato">o</span>
+          </h1>
+          <div id="flip-note" onClick={this.flip}>
+            <div id="flip-note-inner">
+              <div id="flip-note-front">
+                <p id="greeting">Dear visitor,</p>
+                <p>My name is Malik Glass</p>
+              </div>
+              <div id="flip-note-back">
+                <p>This is the back</p>
+              </div>
+            </div>
+          </div>
+          {/* <a href="https://github.com/Malik-G" target="_blank"><img src="images/github-10-xxl.png" className="github-logo" /></a> */}
 
-					<div className="infoContainer">
-						<h2 className="colorOrange">Description</h2>
+        </div>
+        <div className="portfolio">
+          <h1 className="big-font">
+            <span className="colorOrange">The </span>
+            <span className="colorSeaGreen"> Port</span>
+            <span className="colorLightBlue">fo</span>
+            <span className="colorTomato">lio</span>
+          </h1>
+        </div>
 
-						<p></p>
-					</div>
-					<div className="infoContainer">
-						<h2 className="colorSeaGreen">See Online</h2>
-						<a href=""><Button variant="contained" color="primary">CODE ON GITHUB</Button></a>
-						<a href=""><Button variant="contained" color="default">WEBSITE</Button></a>
-					</div>
-					<div className="infoContainer">
-						<h2 className="colorTomato">Technologies</h2>
-						{this.props.reduxState.tags.map(tag =>
-							<span>*{tag.name} </span>
-						)}
-					</div>
-
-					<div className="parallax-todo"></div>
-
-					<div className="infoContainer">
-						<h2 className="colorOrange">Description</h2>
-
-						<p>"".description}</p>
-					</div>
-					<div className="infoContainer">
-						<h2 className="colorSeaGreen">See Online</h2>
-						<a href=""><Button variant="contained" color="primary">CODE ON GITHUB</Button></a>
-						<a href=""><Button variant="contained" color="default">WEBSITE</Button></a>
-					</div>
-					<div className="infoContainer">
-						<h2 className="colorTomato">Technologies</h2>
-						{this.props.reduxState.tags.map(tag =>
-							<span>*{tag.name} </span>
-						)}
-					</div>
-
-					<div className="parallax-book"></div>
-
-            )}
-         </section>
-      )
-   }
+      </section>
+    )
+  }
 }
 
 const mapReduxStateToProps = (reduxState) => {
-   return { reduxState };
+  return { reduxState };
 }
 
 //put inside of an object to use withStyles object
@@ -156,3 +132,50 @@ const mapReduxStateToProps = (reduxState) => {
 
 export default connect(mapReduxStateToProps)(ProjectPage);
 //export default withRouter(connect(mapReduxStateToProps)(withStyles(styling)(ProjectPage)));
+
+{/* <div className="parallax-reciperiot"></div>
+
+
+  <div className="infoContainer">
+    <h2 className="colorOrange">Description</h2>
+  </div>
+  <div className="infoContainer">
+    <h2 className="colorSeaGreen">See Online</h2>
+    <a href=""><Button variant="contained" color="primary">CODE ON GITHUB</Button></a>
+    <a href=""><Button variant="contained" color="default">WEBSITE</Button></a>
+  </div>
+  <div className="infoContainer">
+    <h2 className="colorTomato">Technologies</h2>
+  </div>
+
+  <div className="parallax-rgb"></div>
+
+  <div className="infoContainer">
+    <h2 className="colorOrange">Description</h2>
+  </div>
+  <div className="infoContainer">
+    <h2 className="colorSeaGreen">See Online</h2>
+    <a href=""><Button variant="contained" color="primary">CODE ON GITHUB</Button></a>
+    <a href=""><Button variant="contained" color="default">WEBSITE</Button></a>
+  </div>
+  <div className="infoContainer">
+    <h2 className="colorTomato">Technologies</h2>
+  </div>
+
+  <div className="parallax-todo"></div>
+
+  <div className="infoContainer">
+    <h2 className="colorOrange">Description</h2>
+
+    <p>"".description}</p>
+  </div>
+  <div className="infoContainer">
+    <h2 className="colorSeaGreen">See Online</h2>
+    <a href=""><Button variant="contained" color="primary">CODE ON GITHUB</Button></a>
+    <a href=""><Button variant="contained" color="default">WEBSITE</Button></a>
+  </div>
+  <div className="infoContainer">
+    <h2 className="colorTomato">Technologies</h2>
+  </div>
+
+  <div className="parallax-book"></div> */}
